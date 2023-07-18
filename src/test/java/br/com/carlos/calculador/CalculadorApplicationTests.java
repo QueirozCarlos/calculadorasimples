14a399f5e2db5cf617dc84a96b0fa7743437e787
Carlos Queiroz <carlos.aqrodrigues@hotmail.com>
2023-07-17 22:25:49 -0300

    CalculadoraSimples

---
+++ src/test/java/br/com/carlos/calculador/CalculadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlos.calculador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
